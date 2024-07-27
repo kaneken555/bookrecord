0386dc9885227b31a611c1f1af0f72385e02c8fb
kaneken <99122116+kaneken555@users.noreply.github.com>
2024-07-28 08:41:48 +0900

    [mod]MySQLに移行

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'my_django_db',
+        'USER': 'django_user',
+        'PASSWORD': 'mysql_pass',
+        'HOST': 'localhost',  # または他のデータベースホスト
+        'PORT': '3306',       # デフォルトのMySQLポート
     }
 }
 
