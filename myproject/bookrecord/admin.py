f20aeecd8c870a42e740970e179c3458e8991fff
kaneken <99122116+kaneken555@users.noreply.github.com>
2024-09-01 14:52:19 +0900

    [add]add genre

--- myproject/bookrecord/admin.py
+++ myproject/bookrecord/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Genre
 
-# Register your models here.
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('genre_name', 'genre_id')  # 表示したいフィールドを指定
+    search_fields = ('genre_name',)  # 検索ボックスを追加
