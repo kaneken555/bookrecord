63a1b57d3f7b861efb49090a51fe6651fec17bea
kaneken <99122116+kaneken555@users.noreply.github.com>
2024-09-01 19:24:55 +0900

    [mod]

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -138,7 +138,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 # STATIC_ROOTの設定を追加
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
